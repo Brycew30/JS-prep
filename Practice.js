891cccb4b3e2c7bd3f06403a6c14bcd53d6ef3f7
Brycew30 <brycew30@gmail.com>
2020-12-03 10:23:38 -0600

    add check prime and reverse strings

--- Practice.js
+++ Practice.js
@@ -1,3 +1,22 @@
+// Reverse words greater than 3 characters
+let str = "Here is a random sentence to test this function"
+let newStr = str.split(" ").map(function(word) {return word.length > 3 ? word.split('').reverse().join('') : word;}).join(' ');
+console.log("Reversed string: " + newStr);
+
+// check prime number
+function isPrime(n){
+  let divisor = 2;
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+console.log("145 is prime?" + isPrime(145));
+
 // power function
 const power = (base, exponent) => {
     let result = 1;
@@ -6,7 +25,7 @@ const power = (base, exponent) => {
     }
     return result;
 };
-console.log(power(2,10));
+console.log("2 to the 10th power is: " + power(2,10));
 
 
 // practice factorial function
@@ -17,7 +36,7 @@ function factorial(n) {
         return factorial(n - 1) * n
     }
 }
-console.log(factorial(7))
+console.log("7 factorial is: " + factorial(7));
 
 // typeof
 console.log(typeof factorial)
@@ -28,5 +47,5 @@ let userNumber = Number(prompt('Pick a number'))
         alert("That's not a number!")
         userNumber = Number(prompt('Pick another number'))
     }
-console.log(userNumber)
+console.log(userNumber + "is the user's number.");
 
